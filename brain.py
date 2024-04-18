8b56b480eebd58f0294f7b271422293a67431280
Brett Palmer <mindsmend@gmail.com>
2024-04-18 23:50:33 +0100

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,18 +1,5 @@
-# PRIVATE AND CONFIDENTIAL [Intellectual Property Of Brett Palmer mince@foldingcircles.co.uk]
-# [No Copying Or Reading Or Use Permitted !]
-"""
-Copyright (c) 2023, Brett Palmer (Mince@foldingcircles.co.uk)
+# Brett Palmer mince@foldingcircles.co.uk
 
-All rights reserved. No permission is granted for anyone, except the software owner, Brett Palmer, to use, copy, modify,
-distribute, sublicense, or otherwise deal with the software in any manner.
-
-Any unauthorized use, copying, or distribution of this software without the explicit written consent of the software
-owner is strictly prohibited.
-
-For permission requests, please contact the software owner, Brett Palmer, at Mince@foldingcircles.co.uk.
-"""
-
-# FoldingCircles Making The Unknown Known
 
 # Deep Learn [try]
 __version__ = "0.0.0001"
